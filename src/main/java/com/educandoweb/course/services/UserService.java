78e464cf47752326501f04be68b933cdebba7d59
Marcelo Bogado <celombc@gmail.com>
2021-10-04 13:15:00 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,31 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getById(id);
+		updatedata(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updatedata(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
